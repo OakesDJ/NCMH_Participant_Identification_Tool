ac0b223710355da863a923f8bd052185e8bcfe68
OakesDJ <103947477+OakesDJ@users.noreply.github.com>
2022-05-06 13:37:46 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,4 +1,22 @@
 
+# Packages
+library(shiny)
+library(readxl)
+library(magrittr)
+library(tidyverse)
+
+# Preliminary Data Cleaning
+data <- read.csv(url("https://raw.githubusercontent.com/OakesDJ/NCMH_Participant_Identification_Tool/main/Participant_Identification_Dummy_Data.csv"), skipNul = F, sep = ",", fill = T) ## Importing data from github. 
+data$study_id <- gsub("[^0-9_]", "", data$study_id) ## Formatting individual variables. 
+data$first_name <- gsub("[^a-z]", "", tolower(data$first_name))
+data$middle_name <- gsub("[^a-z,]", "", tolower(data$middle_name))
+data$last_name <- gsub("[^a-z-]", "", tolower(data$last_name))
+data$dob <- gsub("[^0-9/]", "", data$dob)
+data$post_code <- gsub("[^A-Za-z0-9]", "", tolower(data$post_code))
+data$address <- tolower(data$address)
+data[colnames(data)] <- lapply(data[colnames(data)], as.character) ## Appling character format to all variables. 
+data[is.na(data)] <- as.character("NA") ## Used to convert NAs into a character format so they are not removed in the GSUB. 
+
 # Shiny Application
 
 shinyApp(
@@ -7,10 +25,9 @@ shinyApp(
     
     ui = fluidPage(
         
-        tags$head(
-            tags$link(rel = "stylesheet", type = "text/css", href = "participant_identification_tool.css")
-        ),
-        
+        #tags$head(
+            #tags$link(rel = "stylesheet", type = "text/css", href = "participant_identification_tool.css")
+        #), ## Created some custom CSS but havent included it in this example. 
         
         titlePanel(title = h3(strong("Participant Identification Tool"))),
         
@@ -22,7 +39,7 @@ shinyApp(
                     inputId = "database_choice",
                     label = "Select Database:", 
                     width = "100%",
-                    choices = c("data", "data_2"),
+                    choices = c("data", "example_of_alt_data"),
                     selected = "data"
                     ),
                 
@@ -58,7 +75,7 @@ shinyApp(
                 textInput(
                     inputId = "dob", 
                     label = "Enter Date of Birth:",
-                    placeholder = "YYYY-MM-DD", 
+                    placeholder = "DD-MM-YYYY", 
                     width = "100%"
                 ),
                 
@@ -111,7 +128,7 @@ shinyApp(
                             tab <- tab %>% filter(str_detect(middle_name, gsub("[^A-Za-z-]", "", tolower(input$middlenames))))
                         
                             if(!is.na(input$dob))
-                                tab <- tab %>% filter(str_detect(dob, gsub("[^0-9-]", "", input$dob)))
+                                tab <- tab %>% filter(str_detect(dob, gsub("[^0-9/]", "", input$dob)))
                                 
                                 if(!is.na(input$postcode))
                                     tab <- tab %>% filter(str_detect(post_code, gsub("[^A-Za-z0-9]", "", tolower(input$postcode))))
@@ -132,12 +149,4 @@ shinyApp(
     }
 
 )
-
-
-
-
-
-
-
-
 
